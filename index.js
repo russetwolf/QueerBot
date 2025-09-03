71154206cd322b782a4b9006b4b48d47721ea070
Alex C <alexccend@gmail.com>
2025-09-03 15:24:13 -0400

    Cooldown handling

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const { token } = require('./config.json');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.cooldowns = new Collection();
+
 client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
@@ -24,6 +26,19 @@ for (const folder of commandFolders) {
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
+	}
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
 
