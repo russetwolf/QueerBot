9d706a0d13303d0a22e00fd77693e1a202bd1606
Alex C <alexccend@gmail.com>
2025-11-24 13:40:33 -0500

    Updating README

--- command-support/souvenir-list.js
+++ command-support/souvenir-list.js
@@ -1,12 +1,19 @@
 module.exports = async (tables, guildId, flags) => {
-	console.log("A");
 	const table = tables.get("souvenirs");
 	const reminders = await table.findAll();
 
 	return reminders.filter(r => {
-		if (r.get('guildId') != guildId) return false;
+		console.log(`Row ${r.id}`)
+		if (r.get('guildId') != guildId) {
+			console.log(`not part of this guild ${r.guildId} =/= ${guildId}`);
+			return false;
+		}
 		flags.forEach( pair => {
-			if (r.get(pair.key) != pair.value) return false;
+			if (r.get(pair.key) != pair.value) {
+				console.log(`pair.key = ${pair.key} | pair.value = ${pair.value} | r.key =  ${r.get(pair.key)}`);
+				return false;
+			}
 		});
+		return true;
 	});
 };
